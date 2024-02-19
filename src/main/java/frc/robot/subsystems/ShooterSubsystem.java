a0df5908fae0cbdc4c5449d3b53d44dae277ce8d
EvanComiciotto-lschs <72627874+EvanComiciotto-lschs@users.noreply.github.com>
2024-02-19 10:05:37 -0500

    Changed Intake and Shooter to falcons

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -6,13 +6,22 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
 
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 public class ShooterSubsystem extends SubsystemBase{
-    private CANSparkMax shooterMotorOne;
-    private CANSparkMax shooterMotorTwo;
+    private WPI_TalonSRX shooterMotorOne;
+    private WPI_TalonSRX shooterMotorTwo;
 
     public ShooterSubsystem() {
-        shooterMotorOne = new CANSparkMax(ShooterConstants.shooterMotorOneID, MotorType.kBrushless);
-        shooterMotorTwo = new CANSparkMax(ShooterConstants.shooterMotorTwoID, MotorType.kBrushless);
+        //shooterMotorOne = new CANSparkMax(ShooterConstants.shooterMotorOneID, MotorType.kBrushless);
+        //shooterMotorTwo = new CANSparkMax(ShooterConstants.shooterMotorTwoID, MotorType.kBrushless);
+
+        shooterMotorOne = new WPI_TalonSRX(ShooterConstants.shooterMotorOneID);
+        shooterMotorOne = new WPI_TalonSRX(ShooterConstants.shooterMotorTwoID);
+
+
     }
 
     public void runShooter(double speed) {
