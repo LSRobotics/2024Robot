05df27347b416ba6a7e0c7cc258c67e17a5db416
GabrielKuz <gabrielkuzowsky2027@lschs.org>
2024-02-25 14:13:19 -0500

    updated pathplanner variables

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -129,24 +129,6 @@ public final class Constants {
         }
     }
 
-    public static final class AutoConstants { //TODO: The below constants are used in the example auto, and must be tuned to specific robot
-        public static final double kMaxSpeedMetersPerSecond = 3;
-        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
-        public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
-        public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
-    
-        public static final double kPXController = 1;
-        public static final double kPYController = 1;
-        public static final double kPThetaController = 1;
-    
-        /* Constraint for the motion profilied robot angle controller */
-        public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
-            new TrapezoidProfile.Constraints(
-                kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
-    }
-
-
-
     public static final class IntakeConstants { // TODO: Update Intake CAN IDs
         public static final int intakeMotorOneID = 13;
         public static final double intakeSpeed = 0.9;
@@ -154,10 +136,8 @@ public final class Constants {
 
     public static final class ElevatorConstants { // TODO: Update Elevator CAN IDs
         public static final int elevatorMotorID = 16;
-
         public static final int elevatorTopLimitChannel = 5;
         public static final int elevatorBottomLimitChannel = 6;
-
         public static final double elevatorSpeed = .5;
     }
 
