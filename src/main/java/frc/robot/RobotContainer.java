1718539e52567d030bbdc3f7de2e8f33733fed02
Ben Bell <110692534+slowpoke111@users.noreply.github.com>
2026-09-24 14:44:52 -0400

    Update swerve

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,12 +1,18 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
 import java.util.function.BooleanSupplier;
 
+import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.playingwithfusion.TimeOfFlight;
 
+import edu.wpi.first.util.MsvcRuntimeException;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -15,6 +21,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import frc.robot.commands.*;
+import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.*;
 import frc.robot.Constants.*;
 
@@ -33,12 +40,16 @@ public class RobotContainer {
     private final CommandXboxController operatorController = new CommandXboxController(1);
 
     /* Subsystems */
-    private final Swerve s_Swerve = new Swerve();
+    private final CommandSwerveDrivetrain mSwerve = TunerConstants.createDrivetrain();
     private final ShooterSubsystem m_shooter = new ShooterSubsystem();
     private final IntakeSubsystem m_intake = new IntakeSubsystem();
     private final IndexerSubsystem m_indexer = new IndexerSubsystem();
     private final LEDSubsystem m_leds = new LEDSubsystem();
     private BooleanSupplier notePresent = () -> notePresent();
+
+    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+            .withDeadband(TunerConstants.kSpeedAt12Volts.in(MetersPerSecond) * 0.1).withRotationalDeadband(RotationsPerSecond.of(0.5).in(RadiansPerSecond) * 0.1) // Add a 10% deadband
+            .withDriveRequestType(com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType.OpenLoopVoltage);
 
     private TimeOfFlight indexBeamBreak = new TimeOfFlight(IndexerConstants.indexBeamBreakChannel);
 
@@ -48,13 +59,12 @@ public class RobotContainer {
      * The container for the robot. Contains subsystems, OI devices, and commands.
      */
     public RobotContainer() {
-        s_Swerve.setDefaultCommand(
-                new TeleopSwerve(
-                        s_Swerve,
-                        () -> SwerveConstants.demoSpeed * -driverController.getLeftY(),
-                        () -> SwerveConstants.demoSpeed * -driverController.getLeftX(),
-                        () -> 0.5 * -driverController.getRightX(),
-                        () -> driverController.leftBumper().getAsBoolean()));
+        mSwerve.setDefaultCommand(
+            mSwerve.applyRequest(() -> drive.withVelocityX(-driverController.getLeftY() * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond)) // Drive
+                    .withVelocityY(-driverController.getLeftX() * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond)) // Drive left with negative X
+                    .withRotationalRate(-driverController.getRightX() * RotationsPerSecond.of(0.5).in(RadiansPerSecond)) // Drive counterclockwise
+            )
+        );
 
         configureButtonBindings();
     }
--- src/main/java/frc/robot/commands/IntakeRunCommand.java
+++ src/main/java/frc/robot/commands/IntakeRunCommand.java
@@ -1,7 +1,6 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import frc.robot.subsystems.IntakeSubsystem;
@@ -10,7 +9,6 @@ import frc.robot.subsystems.LEDSubsystem;
 import frc.robot.Constants.LEDConstants;
 import java.util.function.BooleanSupplier;
 import edu.wpi.first.wpilibj2.command.Command;
-
 public class IntakeRunCommand extends Command {
   private final IntakeSubsystem m_intake;
   private final IndexerSubsystem m_indexer;
